88e400ddcb04095bd05418cd112a7555d890393b
aroacefreak <jacob@healthtrack.com.au>
2022-06-01 14:56:12 +1000

    Added publish to web assembly project

--- BookStoreApp.Blazor.WebAssembly.UI/Program.cs
+++ BookStoreApp.Blazor.WebAssembly.UI/Program.cs
@@ -16,7 +16,13 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7186") });
+var baseAddress = "https://localhost:7186";
+if (builder.HostEnvironment.IsProduction())
+{
+	baseAddress = "https://embertutorialbookstoreappapi.azurewebsites.net/";
+}
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
 
 builder.Services.AddBlazoredLocalStorage();
 
